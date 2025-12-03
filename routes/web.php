3f21c780363df905aec22b11ee2af916ab9a02dd
RahulRSharmaji <rahul1998sharmaji@gmail.com>
2025-12-03 19:20:14 +0530

    user dashboard and vendor dashboard panel

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
+use App\Http\Controllers\Frontend\UserProfileController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +21,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index']);
 
-Route::get('/dashboard', function () {
-    return view('frontend.dashboard.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -32,3 +30,10 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('admin/login',[AdminController::class,'login'])->name('admin.login');
+
+Route::group(['middleware' =>['auth','verified'], 'prefix' => 'user', 'as' => 'user.'], function(){
+    Route::get('/dashboard',[UserDashboardController::class,'index'])->name('dashboard');
+    Route::get('/profile',[UserProfileController::class,'index'])->name('profile');
+    Route::post('/profile/update',[UserProfileController::class,'updateProfile'])->name('profile.update');
+    Route::post('/profile/update/password',[UserProfileController::class,'updatePassword'])->name('profile.update.password');
+});
